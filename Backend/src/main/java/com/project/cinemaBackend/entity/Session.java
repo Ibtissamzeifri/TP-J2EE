e56829efa6a1dfaf4e3568a89e66b683cbe08eaf
Ibtissame Zeifri <108102505+Ibtissamzeifri@users.noreply.github.com>
2023-05-31 12:05:22 +0100

    Add files via upload

---
+++ Backend/src/main/java/com/project/cinemaBackend/entity/Session.java
@@ -0,0 +1,34 @@
+package com.project.cinemaBackend.entity;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Date;
+
+@Entity
+public class Session implements Serializable {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Temporal(TemporalType.TIME)
+    private Date hourStart;
+    public Session() {
+    }
+    public Session(Date hourStart) {
+        this.hourStart = hourStart;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Date getHourStart() {
+        return hourStart;
+    }
+
+    public void setHourStart(Date hourStart) {
+        this.hourStart = hourStart;
+    }
+}
